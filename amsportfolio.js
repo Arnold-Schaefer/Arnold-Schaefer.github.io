ca253eafd4ecb8a5edbe2433ee47dc81f1beeecd
Arnold <arnoldschaefer@icloud.com>
2021-03-21 19:26:02 -0400

    Initial Portfolio Project

---
+++ amsportfolio.js
@@ -0,0 +1,7 @@
+function goToGitHub() {
+	window.open('https://github.com/Arnold-Schaefer', "_blank");
+}
+
+function goToLinkedIn() {
+	window.open('https://www.linkedin.com/in/arnoldschaefer/', "_blank");
+}
